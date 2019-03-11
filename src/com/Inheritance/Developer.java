293ca6b76c86540cb8ea09a8c6687385215e4952
aakashshakya <akashraj.shakya@esewa.com.np>
2019-03-11 12:06:53 +0545

    Refactoring code of Sushant vai.

--- src/com/Inheritance/Developer.java
+++ src/com/Inheritance/Developer.java
@@ -1,8 +1,29 @@
 package com.Inheritance;
 
 public class Developer extends Employee {
-    String department = "Java";
-    String position = "Computer Engineer";
+    String department;
+    String position;
 
+    Developer(){}
 
+    public Developer(String department, String position) {
+        this.department = department;
+        this.position = position;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
 }
