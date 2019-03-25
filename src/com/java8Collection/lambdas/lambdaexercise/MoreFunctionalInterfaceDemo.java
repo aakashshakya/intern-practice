4a9d99dcf702d6aac11d0a1271748693d21ccdc2
sushantkandel1995 <peaceandray@gmail.com>
2019-03-25 10:22:09 +0545

    Exception handling in the Lambda

--- src/com/java8Collection/lambdas/lambdaexercise/MoreFunctionalInterfaceDemo.java
+++ src/com/java8Collection/lambdas/lambdaexercise/MoreFunctionalInterfaceDemo.java
@@ -4,42 +4,46 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
-public class DemoUsingJava8 {
-    public static void main(String args[]){
+public class MoreFunctionalInterfaceDemo {
+    public static void main(String args[]) {
         List<Person> people = Arrays.asList(
                 new Person("Sushant", "Kandel", 24),
                 new Person("Kamal", "Poudel", 25),
                 new Person("Hari", "Sapkota", 27),
-                new Person("Shyam", "Khanal", 26),
+                new Person("Keshav", "Khanal", 26),
                 new Person("Sankhar", "Chapagain", 25),
                 new Person("Saira", "Chandra", 21)
         );
         //sort list by last name
-        Collections.sort(people,Comparator.comparing(Person::getLastName));
+        Collections.sort(people, Comparator.comparing(Person::getLastName));
 
         // Create a methods that prints all elements in the list.
-        printAll(people);
+        printAll(people, p -> System.out.println(p));
         //create a lambda expression that prints all people that have last name beginning with C.
-        performConditionally(people, p->p.getLastName().startsWith("C") );
+        performConditionally(people, p -> p.getLastName().startsWith("C"), p -> System.out.println(p.getFirstName()));
 
         //create method that print all people that print first name beginning with s
-        performConditionally(people, p->p.getFirstName().startsWith("S") );
+        performConditionally(people, p -> p.getFirstName().startsWith("S"), p -> System.out.println(p));
 
     }
 
-    public static void performConditionally(List<Person> people, Condition condition) {
+    public static void performConditionally(List<Person> people, Predicate<Person> predicate, Consumer<Person> consumer) {
         for (Person p : people) {
-            if (condition.test(p)) {
-                System.out.println(p);
+            if (predicate.test(p)) {
+                consumer.accept(p);
             }
         }
     }
 
-    public static void printAll(List<Person> people) {
+    public static void printAll(List<Person> people, Consumer<Person> consumer) {
         for (Person p : people) {
-            System.out.println(p);
+            consumer.accept(p);
         }
     }
-    }
+}
+
+
 
